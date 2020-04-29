c497742879b3fab9823c6cff1f93aab943f62479
The Bimmy's <bimmys@air-di-the.home>
2020-04-29 09:36:52 +0100

    added angular material card

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {A11yModule} from '@angular/cdk/a11y';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   exports: [
     A11yModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule
   ]
 })
 export class MaterialModule {}
