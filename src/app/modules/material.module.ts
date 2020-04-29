a24f6ce07ee5344900008bf3030975e287a9addf
The Bimmy's <bimmys@air-di-the.home>
2020-04-29 11:22:02 +0100

    fixed header and footer style

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {A11yModule} from '@angular/cdk/a11y';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   exports: [
     A11yModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule {}
